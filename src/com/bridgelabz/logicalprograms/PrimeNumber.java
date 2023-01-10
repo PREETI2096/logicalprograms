5a8b0b4f89eb15cf6281ca845bc72f6e6cb8c842
PREETI2096 <108446384+PREETI2096@users.noreply.github.com>
2023-01-10 14:16:34 +0530

    Added : logical program

--- src/com/bridgelabz/logicalprograms/PrimeNumber.java
+++ src/com/bridgelabz/logicalprograms/PrimeNumber.java
@@ -3,7 +3,23 @@ package com.bridgelabz.logicalprograms;
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("Enter a number:");
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		boolean isPrime = true;
+		for(int i = 2;i <= number/2; i++)
+		{
+			if (number % i == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+		if(isPrime )
+			System.out.println(number+ " is Prime number");
+		else
+			System.out.println(number+ " is not prime number");
+	
 
 	}
 

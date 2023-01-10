058535bbccc29fc7f140999295702506ddc53f06
PREETI2096 <108446384+PREETI2096@users.noreply.github.com>
2023-01-10 14:17:33 +0530

    Added : logical program

--- src/com/bridgelabz/logicalprograms/ReverseNumber.java
+++ src/com/bridgelabz/logicalprograms/ReverseNumber.java
@@ -3,7 +3,18 @@ package com.bridgelabz.logicalprograms;
 public class ReverseNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number:");
+		int num = sc.nextInt();
+		int reverse = 0;
+		int rem;
+		while(num != 0)
+		{
+			rem = num%10;
+			reverse = reverse * 10 + rem;
+			num /= 10;
+		};
+		System.out.println("Reversed number : " + reverse);
 
 	}
 

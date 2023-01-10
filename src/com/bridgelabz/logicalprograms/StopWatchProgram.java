703f11f16d9044af382af6132378e5ef8a46a813
PREETI2096 <108446384+PREETI2096@users.noreply.github.com>
2023-01-10 14:18:37 +0530

    Added : logical program

--- src/com/bridgelabz/logicalprograms/StopWatchProgram.java
+++ src/com/bridgelabz/logicalprograms/StopWatchProgram.java
@@ -3,7 +3,19 @@ package com.bridgelabz.logicalprograms;
 public class StopWatchProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		char input = ' ';
+		long start = 0;
+		long end = 0;
+		double timeElapsed = 0;
+		System.out.println("Enter any Character to start stopwatch:");
+		Scanner sc = new Scanner(System.in);
+		input = sc.next().charAt(0);
+		start = System.currentTimeMillis();
+		System.out.println("Enter any character to stop stopwatch:");
+		input = sc.next().charAt(0);
+        end = System.currentTimeMillis();
+        timeElapsed = (end - start)/1000;
+        System.out.println("Time elapsed:"+ timeElapsed+"sec");
 
 	}
 
